cb8db926900d01a446482bf7babfd19fdb94e4ba
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-04-24 16:27:26 -0700

    when preorder becomes live, active_since set to current time

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -20,4 +20,18 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
     }
     return order;
   },
+
+  async update(ctx) {
+    const order = await super.update(ctx);
+    if (order.data.attributes.status !== "PRE-EVENT") {
+      const { id } = order.data;
+      const datedOrder = await strapi.entityService.update('api::order.order', id, {
+        data: {
+          active_since: new Date()
+        }
+      }); 
+      return datedOrder;
+    }
+    return order;
+  }
 }));
