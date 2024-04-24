679a37b89b99fb9b1345230774243f66cbad6589
Aryan Singh <aryan.singh.lamba@gmail.com>
2024-04-24 16:24:04 -0700

    active_since column, live orders set to active since current time

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+  async create(ctx) {
+    const order = await super.create(ctx);
+    if (order.data.attributes.status !== "PRE-EVENT") {
+      const { id } = order.data;
+      const datedOrder = await strapi.entityService.update('api::order.order', id, {
+        data: {
+          active_since: new Date()
+        }
+      }); 
+      return datedOrder;
+    }
+    return order;
+  },
+}));
