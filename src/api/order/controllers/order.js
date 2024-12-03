c351a58e50e70d7520ad2e50704e85413f8178af
extreme-coder <aryan.singh.lamba@gmail.com>
2024-12-03 14:00:58 -0800

    order post endpoint populates order-items

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 'use strict';
 
 /**
@@ -9,13 +10,29 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::order.order', ({ strapi }) => ({
   async create(ctx) {
     const order = await super.create(ctx);
+    if (ctx.request.body.data.order_items && ctx.request.body.data.order_items.length > 0) {
+      const orderItems = await Promise.all(ctx.request.body.data.order_items.map(async (item) =>
+        await strapi.entityService.create('api::order-item.order-item', { 
+          data: {
+            item: item.item, 
+            toppings: item.toppings,
+            publishedAt: new Date()
+          }
+        })
+      ))
+      await strapi.entityService.update('api::order.order', order.data.id, {
+        data: {
+          items: orderItems.map(item => item.id)
+        }
+      });
+    }
     if (order.data.attributes.status !== "PRE-EVENT") {
       const { id } = order.data;
       const datedOrder = await strapi.entityService.update('api::order.order', id, {
         data: {
           active_since: new Date()
         }
-      }); 
+      });
       return datedOrder;
     }
     return order;
@@ -29,7 +46,7 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
         data: {
           active_since: new Date()
         }
-      }); 
+      });
       return datedOrder;
     }
     return order;
